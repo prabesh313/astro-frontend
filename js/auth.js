4ceedd82e591d4f3d9f969962b1ea128e968a32b
prabesh313 <prabespaudel57@gmail.com>
2026-04-16 18:58:48 +0545

    resolved profile API routing and frontend integration issues

--- js/auth.js
+++ js/auth.js
@@ -1,3 +1,7 @@
+function getToken() {
+    return localStorage.getItem("access_token");
+}
+
 function saveTokens(access,Refresh){
     localStorage.setItem("access_token",access);
     localStorage.setItem("refresh_token",Refresh);
@@ -17,16 +21,16 @@ function updateNavbar(){
     const loggedIn=isLoggedIn();
     const loginLink=document.getElementById("nav-login");
     const logoutlink=document.getElementById("nav-logout");
-    const kubdaliLink = document.getElementById("nav-kundali");
+    const kundaliLink = document.getElementById("nav-kundali");
 
     if(loggedIn){
         loginLink.style.display="none";
         logoutlink.style.display="block";
-        kubdaliLink.style.display="block";
+        kundaliLink.style.display="block";
     }else{
         loginLink.style.display="block";
         logoutlink.style.display="none";
-        kubdaliLink.style.display="none";
+        kundaliLink.style.display="none";
     }
 }
 
